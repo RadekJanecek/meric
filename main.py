9f3ac75adb6e4344ec8506ba251f1ae198383834
Radek Janeček <83546285+RadekJanecek@users.noreply.github.com>
2021-12-02 09:19:48 +0100

    změna

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-cas = 0
+Bpressed = 0
 
 def on_button_pressed_b():
-    global cas
-    cas = randint(250, 3000)
-    music.play_tone(Note.C,cas)
+    global Bpressed
+    Bpressed = randint(250, 3000)
+    music.play_tone(Note.D,Bpressed)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_button_pressed_a():
-    music.play_tone(Note.C,cas)
+    music.play_tone(Note.D,Bpressed)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 basic.show_icon(IconNames.HEART)
--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
-let cas = 0
+let Bpressed = 0
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
     
-    cas = randint(250, 3000)
-    music.playTone(Note.C, cas)
+    Bpressed = randint(250, 3000)
+    music.playTone(Note.D, Bpressed)
 })
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    music.playTone(Note.C, cas)
+    music.playTone(Note.D, Bpressed)
 })
 basic.showIcon(IconNames.Heart)
